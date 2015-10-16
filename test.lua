a9f445f17ffaef3e8a6818ec99a05222b3b4f0ef
rocaltair <rocaltair@gmail.com>
2015-10-16 11:05:00 +0800

    update test.lua

--- test.lua
+++ test.lua
@@ -1,27 +1,31 @@
 ldump = require "ldump"
 
-t1 = {
-	a = 1,
-	x = "5",
-	list = {1,2,3,4,5},
+local tables = {
+	t1 = {
+		a = 1,
+		x = "5",
+		list = {1,2,3,4,5},
+	},
+	t2 = {
+		function () end,
+	},
+	t3 = {
+		[{}] = 5,
+	},
 }
 
-local s, len = ldump.dump(t1)
-print("len", len)
-print("t1", s)
-print("t1 in line", ldump.dump_in_line(t1))
+table.foreach(tables, function(tname, tbl)
+	local ok = ldump.check(tbl)
+	if not ok then
+		print(string.format("tname=%s check failed", tname))
+	else
+		print(string.format("tname=%s check ok", tname))
+		local s, len = ldump.dump(tables.t1)
+		print("len of " .. tname, len)
+		print("dump of " .. tname, s)
+		print(tname .. " in line", ldump.dump_in_line(tbl))
+	end
+end)
 
-
-t2 = {
-	function () end,
-}
-
-t3 = {
-	[{}] = 5,
-}
-
-print("check t1", ldump.check(t1))
-print("check t2", ldump.check(t2))
-print("check t3", ldump.check(t3))
-print("check nil", ldump.check())
+print("check nil", ldump.check()) -- this will emit an error for non args in ldump.check
 
